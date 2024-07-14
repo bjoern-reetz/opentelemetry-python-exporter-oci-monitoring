da9fbd4c90ee97dc99e31cd835a2aea42eccb2d0
Björn Reetz <git@bjoern-reetz.de>
2024-07-14 22:14:03 +0200

    test: test that non-empty metrics_data is sent

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -1,14 +1,41 @@
 from unittest.mock import NonCallableMock
 
-from opentelemetry.sdk.metrics.export import MetricsData
+from oci.monitoring.models import PostMetricDataDetails
+from oci.response import Response
+from opentelemetry.sdk.metrics.export import MetricExportResult, MetricsData
 from opentelemetry_exporter_oci_monitoring import OCIMetricsExporter
 
 
 def test_exporter(
     monitoring_client: NonCallableMock, metrics_data: MetricsData
 ) -> None:
-    exporter = OCIMetricsExporter(
-        monitoring_client, namespace="foo", resource_group="bar", compartment_id="baz"
+    namespace = "foo"
+    resource_group = "bar"
+    compartment_id = "baz"
+
+    def validate_post_metric_data(
+        post_metric_data_details: PostMetricDataDetails,
+    ) -> Response:
+        assert (
+            post_metric_data_details.batch_atomicity
+            == PostMetricDataDetails.BATCH_ATOMICITY_ATOMIC
+        )
+        assert len(post_metric_data_details.metric_data) > 0
+
+        return monitoring_client.post_metric_data.return_value
+
+    monitoring_client.configure_mock(
+        **{"post_metric_data.side_effect": validate_post_metric_data}
     )
 
-    exporter.export(metrics_data)
+    exporter = OCIMetricsExporter(
+        monitoring_client,
+        namespace=namespace,
+        resource_group=resource_group,
+        compartment_id=compartment_id,
+    )
+
+    result = exporter.export(metrics_data)
+    assert result == MetricExportResult.SUCCESS
+
+    monitoring_client.post_metric_data.assert_called_once()
